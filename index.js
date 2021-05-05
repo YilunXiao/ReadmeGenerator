52f4d9c9301c7b5e86c2d93c64e7040642d94149
Yilun Xiao <yilunxiao2@gmail.com>
2021-05-04 23:53:33 -0400

    made first inquiry and logged it to README file

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 // DEPENDENCIES
 const fs = require('fs'); // Files system
 const inquirer = require('inquirer'); // inquirer npm
+const fileName = 'README-template.md';
 
 inquirer
     .prompt([
         /* Pass your questions in here */
+        {
+            type: 'input',
+            message: 'What is your project called?',
+            name: 'title',
+        },
     ])
     .then(answers => {
         // Use user feedback for... whatever!!
+        const fileContent = `# ${answers.title}`
+        fs.appendFile(fileName, fileContent, (err) =>
+            err ? console.error(err) : console.log('Commit logged!')
+        );
     })
     .catch(error => {
     if(error.isTtyError) {
